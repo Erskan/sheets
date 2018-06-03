c27ef653165ff8520e931be69d39bbddd5fb5088
Erik Mattsson Mårn <erikmattssonmarn@gmail.com>
2018-06-03 16:08:42 +0200

    Started work on validation

---
+++ SheetsApi/Sheets/SheetValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SheetsApi.Sheets
+{
+    public class SheetValidator : AbstractValidator<Sheet>
+    {
+        public SheetValidator()
+        {
+            RuleFor(sheet => sheet.Attacks).GreaterThanOrEqualTo(0);
+            RuleFor(sheet => sheet.WeaponSkill).GreaterThanOrEqualTo(0);
+            RuleFor(sheet => sheet.BallisticSkill).GreaterThanOrEqualTo(0);
+            RuleFor(sheet => sheet.InvulnerableSave).GreaterThanOrEqualTo(0);
+            RuleFor(sheet => sheet.Save).GreaterThanOrEqualTo(0);
+            RuleFor(sheet => sheet.Wounds).GreaterThanOrEqualTo(0);
+            RuleFor(sheet => sheet.Leadership).GreaterThanOrEqualTo(0);
+            RuleFor(sheet => sheet.Movement).GreaterThanOrEqualTo(0);
+            RuleFor(sheet => sheet.Strength).GreaterThanOrEqualTo(0);
+            RuleFor(sheet => sheet.Toughness).GreaterThanOrEqualTo(0);
+            RuleFor(sheet => sheet.Name).NotEmpty();
+        }
+    }
+}
--- SheetsApi/Sheets/SheetsController.cs
+++ SheetsApi/Sheets/SheetsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
 using SheetsApi.Shared.Interfaces;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
@@ -28,18 +29,36 @@ namespace SheetsApi.Sheets
         [SwaggerResponse(400)]
         public async Task<IActionResult> GetSheet(int id)
         {
+            Log.Information("GET sheets/{id} called from {RemoteIpAddress}.", id, HttpContext.Connection.RemoteIpAddress);
             var sheet = await _sheetService.GetAsync(id);
             if(sheet == null)
             {
+                Log.Warning("sheets/{id} NOT found. Returning 404.", id);
                 return NotFound();
             }
+            Log.Information("sheets/{id} found. Returning sheet.", id);
             return Ok(sheet);
         }
 
         [HttpPost]
-        [SwaggerResponse(200, typeof(int))]
-        public Task<IActionResult> CreateSheet([FromBody] Sheet sheet)
+        [SwaggerResponse(201, typeof(int))]
+        [SwaggerResponse(400, typeof(FluentValidation.Results.ValidationResult))]
+        public async Task<IActionResult> CreateSheet([FromBody] Sheet sheet)
         {
+            Log.Information("POST sheets/ called from {RemoteIpAddress}.", HttpContext.Connection.RemoteIpAddress);
+            if(sheet == null)
+            {
+                return StatusCode(400);
+            }
+
+            // TODO: Fix validation. Currently not validating anything..
+            var validator = new SheetValidator();
+            var validationResult = validator.Validate(sheet);
+            if (!validationResult.IsValid)
+            {
+                return StatusCode(400, validationResult);
+            }
+
             throw new NotImplementedException();
         }
 
