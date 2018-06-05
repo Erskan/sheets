4b7044017223e53813e645f6daaf3847a9ae3405
Erik Mattsson Mårn <erik.mattssonmarn@consid.se>
2018-06-05 16:32:20 +0200

    Added unit test project.

---
+++ SheetsApi.UnitTests/SheetsTests/SheetsControllerTests.cs
@@ -0,0 +1,17 @@
+using System;
+using SheetsApi.Shared.Interfaces;
+using Xunit;
+
+namespace SheetsApi.UnitTests
+{
+    public class SheetsControllerTests
+    {
+        [Fact]
+        public void GetAll_ServiceReturnsNoSheets_ShouldReturn404()
+        {
+            //var mockSheets = new Mock<ISheetService>();
+            // TODO: Without internet on the train atm. Import Moq and get testing!
+            return;
+        }
+    }
+}
