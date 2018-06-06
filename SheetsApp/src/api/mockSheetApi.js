2412dd3eb5b0384994e0949cf91ca1645da387e3
Erik Mattsson Mårn <erikmattssonmarn@gmail.com>
2018-06-06 18:44:42 +0200

    Renamed mock api and don't use SheetsPage for sheet management.

--- SheetsApp/src/api/mockSheetApi.js
+++ SheetsApp/src/api/mockSheetApi.js
@@ -1,44 +1,41 @@
 import delay from './delay';
 
-// This file mocks a web API by working with the hard-coded data below.
-// It uses setTimeout to simulate the delay of an AJAX call.
-// All calls return promises.
-const courses = [
+const sheets = [
   {
-    id: "react-flux-building-applications",
-    title: "Building Applications in React and Flux",
+    id: 1,
+    name: "Building Applications in React and Flux",
     watchHref: "http://www.pluralsight.com/courses/react-flux-building-applications",
     authorId: "cory-house",
     length: "5:08",
     category: "JavaScript"
   },
   {
-    id: "clean-code",
-    title: "Clean Code: Writing Code for Humans",
+    id: 2,
+    name: "Clean Code: Writing Code for Humans",
     watchHref: "http://www.pluralsight.com/courses/writing-clean-code-humans",
     authorId: "cory-house",
     length: "3:10",
     category: "Software Practices"
   },
   {
-    id: "architecture",
-    title: "Architecting Applications for the Real World",
+    id: 3,
+    name: "Architecting Applications for the Real World",
     watchHref: "http://www.pluralsight.com/courses/architecting-applications-dotnet",
     authorId: "cory-house",
     length: "2:52",
     category: "Software Architecture"
   },
   {
-    id: "career-reboot-for-developer-mind",
-    title: "Becoming an Outlier: Reprogramming the Developer Mind",
+    id: 4,
+    name: "Becoming an Outlier: Reprogramming the Developer Mind",
     watchHref: "http://www.pluralsight.com/courses/career-reboot-for-developer-mind",
     authorId: "cory-house",
     length: "2:30",
     category: "Career"
   },
   {
-    id: "web-components-shadow-dom",
-    title: "Web Component Fundamentals",
+    id: 5,
+    name: "Web Component Fundamentals",
     watchHref: "http://www.pluralsight.com/courses/web-components-shadow-dom",
     authorId: "cory-house",
     length: "5:10",
@@ -51,57 +48,57 @@ function replaceAll(str, find, replace) {
 }
 
 //This would be performed on the server in a real app. Just stubbing in.
-const generateId = (course) => {
-  return replaceAll(course.title, ' ', '-');
+const generateId = (sheet) => {
+  return replaceAll(sheet.name, ' ', '-');
 };
 
-class CourseApi {
-  static getAllCourses() {
+class SheetApi {
+  static getAllSheets() {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve(Object.assign([], courses));
+        resolve(Object.assign([], sheets));
       }, delay);
     });
   }
 
-  static saveCourse(course) {
-    course = Object.assign({}, course); // to avoid manipulating object passed in.
+  static saveSheet(sheet) {
+    sheet = Object.assign({}, sheet); // to avoid manipulating object passed in.
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         // Simulate server-side validation
-        const minCourseTitleLength = 1;
-        if (course.title.length < minCourseTitleLength) {
-          reject(`Title must be at least ${minCourseTitleLength} characters.`);
+        const minSheetNameLength = 1;
+        if (sheet.title.length < minSheetNameLength) {
+          reject(`Name must be at least ${minSheetNameLength} characters.`);
         }
 
-        if (course.id) {
-          const existingCourseIndex = courses.findIndex(a => a.id == course.id);
-          courses.splice(existingCourseIndex, 1, course);
+        if (sheet.id) {
+          const existingSheetIndex = sheets.findIndex(a => a.id == sheet.id);
+          sheets.splice(existingSheetIndex, 1, sheet);
         } else {
           //Just simulating creation here.
           //The server would generate ids and watchHref's for new courses in a real app.
           //Cloning so copy returned is passed by value rather than by reference.
-          course.id = generateId(course);
-          course.watchHref = `http://www.pluralsight.com/courses/${course.id}`;
-          courses.push(course);
+          sheet.id = generateId(sheet);
+          sheet.watchHref = `http://www.pluralsight.com/courses/${sheet.id}`;
+          sheet.push(sheet);
         }
 
-        resolve(course);
+        resolve(sheet);
       }, delay);
     });
   }
 
-  static deleteCourse(courseId) {
+  static deleteSheet(sheetId) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        const indexOfCourseToDelete = courses.findIndex(course => {
-          course.id == courseId;
+        const indexOfSheetToDelete = sheets.findIndex(sheet => {
+          sheet.id == sheetId;
         });
-        courses.splice(indexOfCourseToDelete, 1);
+        sheets.splice(indexOfSheetToDelete, 1);
         resolve();
       }, delay);
     });
   }
 }
 
-export default CourseApi;
+export default SheetApi;
--- SheetsApp/src/components/sheet/SheetsPage.js
+++ SheetsApp/src/components/sheet/SheetsPage.js
@@ -6,22 +6,6 @@ import * as sheetActions from '../../actions/sheetActions';
 class SheetsPage extends React.Component{
     constructor(props, context) {
         super(props, context);
-        this.state = {
-            sheet: { name: "" }
-        };
-
-        this.onNameChange = this.onNameChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-    
-    onNameChange(event) {
-        const sheet = this.state.sheet;
-        sheet.name = event.target.value;
-        this.setState({sheet: sheet});
-    }
-
-    onClickSave() {
-        this.props.actions.createSheet(this.state.sheet);
     }
 
     sheetsRow(sheet, index) {
@@ -33,9 +17,6 @@ class SheetsPage extends React.Component{
             <div>
                 <h1>Sheets</h1>
                 {this.props.sheets.map(this.sheetsRow)}
-                <h2>Create a new sheet</h2>
-                <input type="text" onChange={this.onNameChange} value={this.state.sheet.name} />
-                <input type="submit" value="Save" onClick={this.onClickSave} />
             </div>
         );
     }
