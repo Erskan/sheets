b09bb0025aab36845d2b24f5a35265a4d2a06608
Erik Mattsson Mårn <erik.mattssonmarn@consid.se>
2018-06-06 14:18:25 +0200

    Added sheets routing

--- SheetsApp/src/components/common/Header.js
+++ SheetsApp/src/components/common/Header.js
@@ -6,6 +6,8 @@ const Header = () => {
         <nav>
             <IndexLink to="/" activeClassName="active">Home</IndexLink>
             {" | "}
+            <Link to="/sheet" activeClassName="active">Sheets</Link>
+            {" | "}
             <Link to="/about" activeClassName="active">About</Link>
         </nav>
     );
--- SheetsApp/src/routes.js
+++ SheetsApp/src/routes.js
@@ -3,10 +3,12 @@ import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import SheetsPage from './components/sheet/SheetsPage';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage} />
         <Route path="about" component={AboutPage} />
+        <Route path="sheet" component={SheetsPage} />
     </Route>
 );
