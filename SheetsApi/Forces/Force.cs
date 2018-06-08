47f99c832cd1d967ac399898a1e7e33b73ebb5d2
Erik Mattsson Mårn <erik.mattssonmarn@consid.se>
2018-06-08 16:37:02 +0200

    Make externally exposed objects a little more interesting.

--- SheetsApi/Forces/Force.cs
+++ SheetsApi/Forces/Force.cs
@@ -1,8 +1,20 @@
-﻿namespace SheetsApi.Forces
+﻿using System.Collections.Generic;
+using SheetsApi.Games;
+using SheetsApi.Sheets;
+
+namespace SheetsApi.Forces
 {
     public class Force
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public IEnumerable<Sheet> Sheets { get; set; }
+        public IEnumerable<Game> Games { get; set; }
+
+        public Force()
+        {
+            Sheets = new List<Sheet>();
+            Games = new List<Game>();
+        }
     }
 }
--- SheetsApi/Games/Game.cs
+++ SheetsApi/Games/Game.cs
@@ -1,8 +1,14 @@
-﻿namespace SheetsApi.Games
+﻿using SheetsApi.Forces;
+
+namespace SheetsApi.Games
 {
     public class Game
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public Force Winner { get; set; }
+        public Force Loser { get; set; }
+        public int? WinnerScore { get; set; }
+        public int? LoserScore { get; set; }
     }
 }
