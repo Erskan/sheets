c38507879e109f1c192747fd918b1934b795e7ba
Erik Mattsson Mårn <erik.mattssonmarn@consid.se>
2018-06-10 16:20:04 +0200

    Some mapping and added automapper config test.

---
+++ SheetsApi.UnitTests/SettingsTests/MappingTests.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SheetsApi.Settings;
+using Xunit;
+
+namespace SheetsApi.UnitTests.SettingsTests
+{
+    public class MappingTests
+    {
+        [Fact]
+        public void EnsureMappingsAreCorrect()
+        {
+            Mapper.Initialize(x => new SheetsMappingProfile());
+            Mapper.AssertConfigurationIsValid();
+            Mapper.Reset();
+        }
+    }
+}
--- SheetsApi/Settings/SheetsMappingProfile.cs
+++ SheetsApi/Settings/SheetsMappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using SheetsApi.Forces;
+using SheetsApi.Games;
 using SheetsApi.Sheets;
 
 namespace SheetsApi.Settings
@@ -8,13 +9,18 @@ namespace SheetsApi.Settings
     {
         public SheetsMappingProfile()
         {
-            CreateMap<SheetModel, Sheet>();
+            CreateMap<SheetModel, Sheet>()
+                .ForMember(m => m.Id, a => a.MapFrom(s => s.SheetId));
             CreateMap<Sheet, SheetModel>()
+                .ForMember(m => m.SheetId, a => a.MapFrom(s => s.Id))
                 .ForMember(m => m.Created, a => a.Ignore())
                 .ForMember(m => m.Modified, a => a.Ignore())
                 .ForMember(m => m.AddedByUser, a => a.MapFrom(s => s /* TODO: Get user from token? */))
                 .ForMember(m => m.ModifiedByUser, a => a.MapFrom(s => s /* TODO: Get user from token? */));
-            CreateMap<ForceModel, Force>();
+            CreateMap<ForceModel, Force>()
+                .ForMember(m => m.Id, a => a.MapFrom(s => s.ForceId));
+            CreateMap<GameModel, Game>()
+                .ForMember(m => m.Id, a => a.MapFrom(s => s.GameId));
         }
     }
 }
